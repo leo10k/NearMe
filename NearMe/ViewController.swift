8b185b83c122464c8c6edf3a1fbde1cd7f72fccf
leo10k <leozinhomacedo41@gmail.com>
2024-03-25 23:21:44 -0300

    feat: create base project

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Leonardo Macedo on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
