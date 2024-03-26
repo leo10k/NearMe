00e325eada1e243aedb68aa25b15236dca575378
leo10k <leozinhomacedo41@gmail.com>
2024-03-26 16:46:40 -0300

    feat: add PlaceAnnotation and CLLocation+Extensions

---
+++ NearMe/Extensions/CLLocation+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  CLLocation+Extensions.swift
+//  NearMe
+//
+//  Created by Leonardo Macedo on 26/03/24.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocation {
+    static var `default`: CLLocation {
+        CLLocation(latitude: 36.063457, longitude: -95.880516)
+    }
+}
+
