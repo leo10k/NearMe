281853cd06b30e71c41275734c1a92ad5bd1bd5e
leo10k <leozinhomacedo41@gmail.com>
2024-03-25 23:37:38 -0300

    feat: add mapview using MKMapView

--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        //map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.red
+        
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        
+        view.addSubview(mapView)
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 
 }
 
